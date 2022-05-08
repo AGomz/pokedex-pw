ef22f44be59e19411d423d5c727eb8b63ff6c5d0
Gomez Angeles <92001030+AGomz@users.noreply.github.com>
2022-05-08 10:07:15 -0300

    Se agrego la carpeta view con el html del header y footer

---
+++ view/header.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- favicon -->
+    <link rel="icon" type="image/x-icon" href="./img/favicon.ico">
+    <!-- Bootsrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estiloIndex.css">
+    <title>Pokédex</title>
+</head>
+
+<body>
+
+<header>
+    <!-- Menu principal -->
+    <!--position-fixed w-100-->
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top" aria-label="Fourth navbar example">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Pokédex</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarsExample04">
+                <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../acciones/login.php">Agregar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../acciones/login.php">Modificar</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav mr-auto mb-2 mb-md-2">
+                    <li class="nav-item px-2">
+                        <a class="btn btn-outline-light" href="login.html">Login</a>
+                    </li>
+                    <li class="nav-item px-2">
+                        <a class="nav-link active" href="registro.html">Registrarse</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+
