35ef38fea6d7afb35ee2e4f2442a22df95aacae3
gabtar <gabrieltarifa@gmail.com>
2022-05-09 16:34:09 -0300

    Agregado modal para confirmación de borrar pokémon

--- acciones/generar_tabla_borrar.php
+++ acciones/generar_tabla_borrar.php
@@ -2,12 +2,33 @@
 
 function generarCeldaModalBorrarPokemon($id)
 {
-    $html = <<<EOF
+    return <<<EOF
     <td>
-    <form id="$id" action="./acciones/borrar_pokemon.php" method="POST">
-        <button class="btn btn-danger btn-sm"name="pokemonId" type="submit" value="$id">Borrar</button>
+    <form action="./acciones/borrar_pokemon.php" method="POST">
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal$id">
+      Borrar
+    </button>
+    
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal$id" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Borrar Pokemón</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            ¿Realmente desea borrar el Pokemón?
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+            <button type="submit" class="btn btn-danger" value="$id" name="pokemonId">Borrar</button>
+          </div>
+        </div>
+      </div>
+    </div> 
     </form>
     </td>
     EOF;
-    return $html;
 }
--- view/footer.php
+++ view/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>Pokedex - Grupo </p>
+    <p>Pokedex - Grupo 10 </p>
 </footer>
 <!-- Bootstrap funciones js -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
